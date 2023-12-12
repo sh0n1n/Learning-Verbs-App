0454e2a63d08e29c544f15440b618714705cc1a7
Artem <dogmaster31072003@gmail.com>
2023-12-12 21:22:54 +0200

    Initial Commit

---
+++ LearningVerbs App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningVerbs App
+//
+//  Created by Melkor on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
