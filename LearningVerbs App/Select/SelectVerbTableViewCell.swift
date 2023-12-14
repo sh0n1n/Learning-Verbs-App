aa3731b715d4d8055a65950b0ab00595f3080344
Artem <dogmaster31072003@gmail.com>
2023-12-14 22:16:55 +0200

    Added SnapKit and some new features

---
+++ LearningVerbs App/Select/SelectVerbTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SelectVerbTableViewCell.swift
+//  LearningVerbs App
+//
+//  Created by Melkor on 12/13/23.
+//
+
+import UIKit
+
+final class SelectVerbTableViewCell: UITableViewCell {
+    
+    enum State {
+        case select, unselect
+    }
+    
+    // MARK: - GUI Variables
+    private lazy var checkboxImageView: UIImageView = {
+        let view = UIImageView()
+        
+        
+        
+        return view
+    }()
+    
+}
