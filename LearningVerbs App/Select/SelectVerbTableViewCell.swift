3b8d58709d95dda594d579314b70d12347b992a1
Artem <dogmaster31072003@gmail.com>
2023-12-14 23:22:28 +0200

    Added some private variables, added Initialization and private methods

--- LearningVerbs App/Select/SelectVerbTableViewCell.swift
+++ LearningVerbs App/Select/SelectVerbTableViewCell.swift
@@ -11,15 +11,90 @@ final class SelectVerbTableViewCell: UITableViewCell {
     
     enum State {
         case select, unselect
+        
+        var image: UIImage {
+            switch self {
+            case .select:
+                return UIImage.checkmark
+            case .unselect:
+                return UIImage(systemName: "circlebadge") ?? UIImage.add
+            }
+        }
     }
     
     // MARK: - GUI Variables
     private lazy var checkboxImageView: UIImageView = {
         let view = UIImageView()
         
-        
+        view.image = State.unselect.image
+        view.contentMode = .center
         
         return view
     }()
     
+    private lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        
+        view.axis = .horizontal
+        view.distribution = .fillEqually
+        view.alignment = .center
+        view.spacing = 5
+        
+        return view
+    }()
+    
+    private lazy var infinitiveView: UIView = UIView()
+    
+    private lazy var infinitiveLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .boldSystemFont(ofSize: 16)
+        
+        return label
+    }()
+    
+    private lazy var translationLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var pastLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 16)
+        
+        return label
+    }()
+    
+    private lazy var participleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 16)
+        
+        return label
+    }()
+    
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    func setupUI() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
 }
